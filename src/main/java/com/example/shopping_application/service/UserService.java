434f12f46f7a5b4bab5ea35aff31293da3238d34
AshotSimonyan91 <ashotsimonyan1991@mail.ru>
2023-05-27 21:53:01 +0400

    add user delete/update from admin

--- src/main/java/com/example/shopping_application/service/UserService.java
+++ src/main/java/com/example/shopping_application/service/UserService.java
@@ -1,12 +1,19 @@
 package com.example.shopping_application.service;
 
 import com.example.shopping_application.entity.User;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
 
-    List<User> findAllCategory();
+    List<User> findAll();
     void remove(int id);
     void save(User user);
+    void update(User user, MultipartFile multipartFile) throws IOException;
+
+    void removeById(int id);
+
+    User findById(int id);
 }
