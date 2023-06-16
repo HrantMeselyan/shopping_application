8a25c29e15cb0c9cddd5a3c92981e07fb3c6ca7b
dell <hrant3307@gmail.com>
2023-06-16 15:34:57 +0400

    search

--- src/main/java/com/example/shopping_application/controller/MainController.java
+++ src/main/java/com/example/shopping_application/controller/MainController.java
@@ -12,10 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -57,9 +54,10 @@ public class MainController {
         return "redirect:/";
     }
 
-    @GetMapping("/search")
-    public String searchPage(@RequestParam("value") String value, ModelMap modelMap) {
+    @GetMapping("/search/{value}")
+    public String searchPage(@PathVariable("value") String value, ModelMap modelMap) {
         modelMap.addAttribute("results", mainService.search(value));
         return "result";
     }
+
 }
