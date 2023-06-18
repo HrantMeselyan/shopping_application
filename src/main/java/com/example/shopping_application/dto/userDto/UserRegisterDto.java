493cfccf0a4f9b45f35054ff21c8b672ffbaea08
dell <hrant3307@gmail.com>
2023-06-18 20:51:03 +0400

    validation

--- src/main/java/com/example/shopping_application/dto/userDto/UserRegisterDto.java
+++ src/main/java/com/example/shopping_application/dto/userDto/UserRegisterDto.java
@@ -24,11 +24,11 @@ public class UserRegisterDto {
     @NotNull
     @Size(min = 2,message = "Surname length it should be min 2 max 10 characters")
     private String surname;
-//    @Email(regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
-//            message = "Email is no valid")
+    @Email(regexp = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$",
+            message = "Email is no valid")
     private String email;
-//    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
-//            message = "Should be min 8 character,include digit and capital letter")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$",
+            message = "Should be min 8 character,include digit and capital letter")
     private String password;
     @NotNull(message = "Gender is required")
     private Gender gender;
